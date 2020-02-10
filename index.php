7953b6b9d29660bc1ef9aed2c4dc8bc939936ccd
Your name <you@example.com>
2020-02-10 12:21:43 +0100

    hier is bericht

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<?php
+function abc(){
+$naam = 'Xander';
+$leeftijd =16;
+$lengte= 1.72;
+$stad= 'den haag';
+
+echo sprintf('ik ben %s, ik ben %s jaar oud en ben %s m lang en ik woon in %s', $naam, $leeftijd, $lengte, $stad);
+}
+abc();;
+?>
+
+
